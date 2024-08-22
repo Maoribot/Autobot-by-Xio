6465a9bdc17c8e38b066754d6eb65bbe746b5473
Maoribot <k1mazumi+705@yandex.com>
2024-08-22 21:19:56 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -19,13 +19,13 @@ module.exports.run = async function({ api, event, args }) {
   const input = args.join(' ');
 
   if (!input) {
-    api.sendMessage(`🤖 𝙴𝙳𝚄𝙲 𝙱𝙾𝚃 𝙰𝙸\n    （„• ֊ •„)♡\n▬▬▬▬▬▬▬▬▬▬▬▬\n\n How can I help you today? `, event.threadID, event.messageID);
+    api.sendMessage(``🤖 𝙴𝙳𝚄𝙲 𝙱𝙾𝚃 𝙰𝙸\n    （„• ֊ •„)♡\n▬▬▬▬▬▬▬▬▬▬▬▬\n\n How can I help you today? `, event.threadID, event.messageID);
     return;
   }
   
   if (input === "clear") {
     try {
-      await axios.post('https://gpt-4-cfgh.onrender.com/clear', { id: event.senderID });
+      await axios.post('https://satomoigpt.onrender.com/clear', { id: event.senderID });
       return api.sendMessage("Chat history has been cleared.", event.threadID, event.messageID);
     } catch {
       return api.sendMessage('An error occurred while clearing the chat history.', event.threadID, event.messageID);
@@ -39,13 +39,14 @@ module.exports.run = async function({ api, event, args }) {
       ? { link: event.messageReply.attachments[0].url }
       : {};
 
-    const { data } = await axios.post('https://gpt-4-cfgh.onrender.com/chat', {
+    const { data } = await axios.post('https://satomoigpt.onrender.com/chat', {
       prompt: input,
       customId: event.senderID,
       ...url
     });
 
-    api.sendMessage(`'🤖 𝙴𝙳𝚄𝙲 𝙱𝙾𝚃 𝙰𝙸\n    （„• ֊ •„)♡\n▬▬▬▬▬▬▬▬▬▬▬▬\n${data.message}\n\nType "ai clear" to reset the conversation.\n▬▬▬▬▬▬▬▬▬▬▬▬\n[📚]|𝗚𝗣𝗧-𝟰`, event.threadID, event.messageID);
+    api.sendMessage(
+'🤖 𝙴𝙳𝚄𝙲 𝙱𝙾𝚃 𝙰𝙸\n    （„• ֊ •„)♡\n▬▬▬▬▬▬▬▬▬▬▬▬\n[${data.message}\n\nType "ai clear" to reset the conversation.]\n▬▬▬▬▬▬▬▬▬▬▬▬\n[📚]|𝗚𝗣𝗧-𝟰 ', event.threadID, event.messageID);
   } catch {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
